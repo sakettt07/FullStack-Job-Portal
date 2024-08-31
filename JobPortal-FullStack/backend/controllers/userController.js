2da400454ab5e08cec5b1919318c188c73688935
saket <saket@saketgupta.com>
2024-08-31 23:32:50 +0530

    completed the user controller for all the possible functionalities might add some more

--- JobPortal-FullStack/backend/controllers/userController.js
+++ JobPortal-FullStack/backend/controllers/userController.js
@@ -1,113 +1,224 @@
-import {User} from "../models/user.model.js";
-import {catchAsyncErrors} from "../middlewares/catchAsync.middleware.js";
+import { User } from "../models/user.model.js";
+import { catchAsyncErrors } from "../middlewares/catchAsync.middleware.js";
 import ErrorHandler from "../middlewares/error.middleware.js";
-import {v2 as cloudinary} from "cloudinary";
-import {sendToken} from "../utils/JwtToken.js"
+import { v2 as cloudinary } from "cloudinary";
+import { sendToken } from "../utils/JwtToken.js";
 
-const registerUser=catchAsyncErrors(async(req,res,next)=>{
-    // TODO:
-    // getting all the data from the body
-    // validate the data and check for the empty fields
-    // Potential check for the existing user if present then return it.
-    // Upload the document resume on the cloudinary then save its url into the database.
-    // generate the cookiee
-    try {
-        const {name,email,phone,address,password,role,firstNiche,secondNiche,thirdNiche,coverLetter}=req.body;
-        if ([name, email, password ,phone,address,role].some((field) => field?.trim() === "")
-          ) {
-            return next(new ErrorHandler("All fields are required",400));
+const registerUser = catchAsyncErrors(async (req, res, next) => {
+  // TODO:
+  // getting all the data from the body
+  // validate the data and check for the empty fields
+  // Potential check for the existing user if present then return it.
+  // Upload the document resume on the cloudinary then save its url into the database.
+  // generate the cookiee
+  try {
+    const {
+      name,
+      email,
+      phone,
+      address,
+      password,
+      role,
+      firstNiche,
+      secondNiche,
+      thirdNiche,
+      coverLetter,
+    } = req.body;
+    if (
+      [name, email, password, phone, address, role].some(
+        (field) => field?.trim() === ""
+      )
+    ) {
+      return next(new ErrorHandler("All fields are required", 400));
+    }
+    if (role === "Job Seeker" && (!firstNiche || !secondNiche || !thirdNiche)) {
+      return next(
+        new ErrorHandler("Please provide your preffered job niches", 400)
+      );
+    }
+    // check for the existing user done
+    const existingUser = await User.findOne({ email, phone });
+    if (existingUser) {
+      return next(
+        new ErrorHandler(
+          "Email is alrerady registered. || Phone number already exist",
+          400
+        )
+      );
+    }
+
+    // uploading the data into the DB
+    const UserData = {
+      name,
+      email,
+      phone,
+      address,
+      password,
+      role,
+      niches: {
+        firstNiche,
+        secondNiche,
+        thirdNiche,
+      },
+      coverLetter,
+    };
+
+    // upload doc on cloudinary
+
+    if (req.files && req.files.resume) {
+      const { resume } = req.files;
+      if (resume) {
+        try {
+          const cloudinaryResponse = await cloudinary.uploader.upload(
+            resume.tempFilePath,
+            { folder: "JobPortal" }
+          );
+          if (!cloudinaryResponse || cloudinaryResponse.error) {
+            return next(new ErrorHandler("Failed to upload resume", 500));
           }
-        if(role==="Job Seeker" &&(!firstNiche ||!secondNiche ||!thirdNiche)){
-            return next(
-                new ErrorHandler("Please provide your preffered job niches",400)
-            );
-        } 
-        // check for the existing user done
-        const existingUser=await User.findOne({email,phone});
-        if(existingUser){
-            return next(new ErrorHandler("Email is alrerady registered. || Phone number already exist",400));
+
+          UserData.resume = {
+            public_id: cloudinaryResponse.public_id,
+            url: cloudinaryResponse.secure_url,
+          };
+        } catch (error) {
+          return next(new ErrorHandler("Failed to upload resume", 500));
         }
-
-        // uploading the data into the DB
-        const UserData={
-            name,email,phone,address,password,role,niches:{
-                firstNiche,secondNiche,thirdNiche,
-            },
-            coverLetter,
-        };
-
-        // upload doc on cloudinary
-
-        if(req.files && req.files.resume){
-            const {resume}=req.files;
-            if(resume){
-                try {
-                    const cloudinaryResponse=await cloudinary.uploader.upload(
-                        resume.tempFilePath,
-                        {folder:"JobPortal"}
-                    );
-                    if(!cloudinaryResponse || cloudinaryResponse.error){
-                        return next(new ErrorHandler("Failed to upload resume",500));
-                    }
-
-                    UserData.resume={
-                        public_id:cloudinaryResponse.public_id,
-                        url:cloudinaryResponse.secure_url,
-                    }
-                } catch (error) {
-                    return next(new ErrorHandler("Failed to upload resume",500))
-                }
-            }
-        }
-        const user=await User.create(UserData);
-        sendToken(user,201,res,"user Registered");
-    } catch (error) {
-        next(error);
-        
+      }
     }
-})
+    const user = await User.create(UserData);
+    sendToken(user, 201, res, "user Registered");
+  } catch (error) {
+    next(error);
+  }
+});
 
-const loginUser=catchAsyncErrors(async(req,res,next)=>{
-    // TODO: get the essentials from the body
-    // then find it in the Database.
-    // convert the password and match
-    // if the user is present then return logged in and change the page 
-    // saved his cookie for the seesion user accessing the site.
-    try {
-        const {email,password,role}=req.body;
-        if(!email ||!password||!role){
-            return next(
-                new ErrorHandler("Email, password ,role are required.",400)
-            );
-        }
-        const user=await User.findOne({email}).select("+password");
-        if(!user){
-            return next(new ErrorHandler("Invalid email or password",400));
-        }
-        const isPasswordMatch=await user.isPasswordCorrect(password);
-        if(!isPasswordMatch){
-            return next(new ErrorHandler("Invalid password",400))
-        }
-        if(user.role!==role){
-            return next(new ErrorHandler("Invalid User role",400))
-        }
-        sendToken(user,200,res,"User logged in successfully");
-    } catch (error) {
-        console.log(error)
-        
+const loginUser = catchAsyncErrors(async (req, res, next) => {
+  // TODO: get the essentials from the body
+  // then find it in the Database.
+  // convert the password and match
+  // if the user is present then return logged in and change the page
+  // saved his cookie for the seesion user accessing the site.
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return next(new ErrorHandler("Email, password ,role are required.", 400));
     }
-})
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return next(new ErrorHandler("Invalid email or password", 400));
+    }
+    const isPasswordMatch = await user.isPasswordCorrect(password);
+    if (!isPasswordMatch) {
+      return next(new ErrorHandler("Invalid password", 400));
+    }
+    if (user.role !== role) {
+      return next(new ErrorHandler("Invalid User role", 400));
+    }
+    sendToken(user, 200, res, "User logged in successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-const logoutUser=catchAsyncErrors(async(req,res,next)=>{
-    res.status(200).cookie("token","",{
-        expires:new Date(Date.now()),
-        httpOnly:true,
-    }).json({
-        success:true,
-        message:"Logged Out successfully."
+const logoutUser = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
     })
-})
-const getUser=catchAsyncErrors(async(req,res,next)=>{
+    .json({
+      success: true,
+      message: "Logged Out successfully.",
+    });
+});
+const getUser = catchAsyncErrors(async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+const updateUserDetails = catchAsyncErrors(async (req, res, next) => {
+  // TODO: get the user details that to be updated from the body.
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: req.body.address,
+    coverLetter: req.body.coverLetter,
+    niches: {
+      firstNiche: req.body.firstNiche,
+      secondNiche: req.body.secondNiche,
+      thirdNiche: req.body.thirdNiche,
+    },
+  };
+  const { firstNiche, secondNiche, thirdNiche } = newUserData.niches;
 
-})
-export {registerUser,loginUser,logoutUser,getUser};
+  if (
+    req.user.role === "Job Seeker" &&
+    (!firstNiche || !secondNiche || !thirdNiche)
+  ) {
+    return next(
+      new ErrorHandler("Please provide your all preferred job niches.", 400)
+    );
+  }
+  //   if he want to update the resume tooo then
+  if (req.files) {
+    // if old resume present get its id and destroy it in the cloudinary.
+    const { resume } = req.files;
+    if (resume) {
+      const currResumeId = req.user.resume.public_id;
+      if (currResumeId) {
+        await cloudinary.uploader.destroy(currResumeId);
+      }
+      // upload the new id for the new resume.
+      const newResume = await cloudinary.uploader.upload(resume.tempFilePath, {
+        folder: "JobPortal",
+      });
+      newUserData.resume = {
+        public_id: newResume.public_id,
+        url: newResume.secure_url,
+      };
+    }
+  }
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    user,
+    message: "Profile Updated.",
+  });
+});
+const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+  const { newPassword, confirmPassword } = req.body;
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old password is incorrect.", 400));
+  }
+
+  if (newPassword !== confirmPassword) {
+    return next(
+      new ErrorHandler("New password & confirm password do not match.", 400)
+    );
+  }
+
+  user.password = newPassword;
+  await user.save();
+  sendToken(user, 200, res, "Password updated successfully.");
+});
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUser,
+  updateUserDetails,
+  updatePassword,
+};
--- JobPortal-FullStack/backend/routes/user.routes.js
+++ JobPortal-FullStack/backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser,getUser } from "../controllers/userController.js";
+import { registerUser,loginUser, logoutUser,getUser, updateUserDetails, updatePassword } from "../controllers/userController.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
 
 const router=Router();
@@ -7,4 +7,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(verifyJWT,logoutUser);
 router.route("/me").get(verifyJWT,getUser);
+router.put("/update/profile",verifyJWT,updateUserDetails)
+router.put("/update/password",verifyJWT,updatePassword)
 export default router
