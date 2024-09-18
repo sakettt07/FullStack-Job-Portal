6ca95b414bc16093f24dec4c50a68bf03180daae
saket <saket@saketgupta.com>
2024-09-19 01:24:59 +0530

    Started with the frontend made folders and files required

--- JobPortal-FullStack/frontend/src/App.jsx
+++ JobPortal-FullStack/frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import "./App.css"
+import "./App.css";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className='bg-zinc-900 w-full h-screen'>
-      <h1 className='text-red-600 text-[3vw]'>Hello Everyone are you looking for a job ?</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </Router>
+    </>
   )
 }
 
