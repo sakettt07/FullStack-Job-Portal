ee7195f056642d56e0089746486fa0660d1197ff
saket <saket@saketgupta.com>
2024-09-27 23:59:18 +0530

    made the spinner and figuiring out the design for the job card

--- JobPortal-FullStack/frontend/src/pages/Jobs.jsx
+++ JobPortal-FullStack/frontend/src/pages/Jobs.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import {useDispatch, useSelector} from "react-redux";
+import { clearAllErrors, fetchJobs } from "../store/slices/jobSlices";
+import Spinner from "../components/Spinner";
+import {toast} from "react-toastify"
 const Jobs = () => {
   const [city, setCity] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
@@ -8,11 +11,43 @@ const Jobs = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
 
 
+  const citiesArray=[];
+  const nicheArray=[];
+
+  const {jobs,loading,error}=useSelector((state)=>state.jobs)
+
+
   const handleCityChange=(city)=>{
     setCity(city);
     setSelectedCity(city);
   }
-  return <div></div>;
+  const handleNicheChange=(niche)=>{
+    setNiche(niche);
+    setSelectedNiche(selectedNiche);
+  }
+
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    if(error){
+      toast.error(error);
+      dispatch(clearAllErrors());
+    }
+    dispatch(fetchJobs(city,niche,searchKeyword));
+
+  },[dispatch,error,city,niche,]);
+
+
+  const handleSearch=()=>{
+    dispatch(fetchJobs(city,niche,searchKeyword));
+  }
+  return <>
+  {
+    loading ? <Spinner />:(
+      <></>
+    )
+  }
+  </>;
 };
 
 export default Jobs;
